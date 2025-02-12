d93ed9b5cc16d12632bb15559a5b1bd9a825c031
alemayehu dabi <alemayehudabi606@gmail.com>
2025-02-12 11:56:35 -0500

    STYLE: right content of detail component

---
+++ client/src/components/DetailPageComponent/RightContent.tsx
@@ -0,0 +1,17 @@
+import SimilarListCard from "./SimilarListCard";
+
+export default function HeadAndTitle() {
+  return (
+    <div className="flex-1 flex flex-col gap-10 ">
+      <span className="font-semibold tracking-wide">Similar Listings</span>
+      <div className="flex flex-col gap-4 mt-10">
+        {/* related listing container */}
+        <SimilarListCard />
+        <SimilarListCard />
+        <SimilarListCard />
+        <SimilarListCard />
+        <SimilarListCard />
+      </div>
+    </div>
+  );
+}
